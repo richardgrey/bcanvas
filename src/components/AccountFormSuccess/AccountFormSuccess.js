b754382e5f2019631931b9f23c6487eef47b6ba3
rich <richie.grey@gmail.com>
2019-09-01 04:57:59 +0400

    Update account form success message

--- src/components/AccountFormSuccess/AccountFormSuccess.js
+++ src/components/AccountFormSuccess/AccountFormSuccess.js
@@ -4,10 +4,11 @@ import './AccountFormSuccess.scss';
 const AccountFormSuccess = () => (
   <div className="account-form-success">
     <div className="account-form-success__pic">
-      <img src={require('./done.png')} alt="Done" />
+      <img src={require('./img/following-the-idea.svg')} alt="All Done" />
     </div>
     <div className="account-form-success__content">
       <h3 className="account-form-success__title">All done!</h3>
+      <p>We have carefully saved all the changes you made.</p>
     </div>
   </div>
 );
